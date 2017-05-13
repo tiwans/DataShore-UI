f86f7dc0b84c41dc5e371dd859113d8a62e93d18
yangf6 <yangf6@uw.edu>
2017-05-13 10:00:46 -0700

    d

--- src/signin.js
+++ src/signin.js
@@ -1,31 +1,43 @@
-"use strict";
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
 
-var signUpForm = document.getElementById("signin-form");
-var emailInput = document.getElementById("email-input");
-var passwordInput = document.getElementById("password-input");
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
 
-authenticateUser();
-
-signUpForm.addEventListener("submit", function(evt) {
-    evt.preventDefault();
-
-  	if (passwordInput.value.length > 5) {
-	    firebase.auth().signInWithEmailAndPassword(emailInput.value, passwordInput.value)
-	        .then(function() {
-	            window.location = "index.html";
-	        })
-	        .catch(function(err) {
-	            alert(err.message);
-	        });
-	    return false;
-	}
 });
 
-function authenticateUser(){
-    firebase.auth().onAuthStateChanged(function(currUser) {
-        if (currUser) {
-            // User is signed in.
-            window.location.href = "index.html";
-        }
-    });
-}
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
